ba722ca09b2f9e9faf818cb0c6d1faef7f505617
Naman0114 <namanprabhatchaturvedi0114@gmail.com>
2024-06-08 14:14:04 +0530

    done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Link } from 'react-scroll';
 import { assets } from '../../assets/assets';
 import './Navbar.css';
 const Navbar = () => {
+    const menuRef=useRef();
     const [pass, setPass] = useState("home");
+
+    const openMenu=()=>{
+        menuRef.current.style.right="0";
+        
+    }
+    const closeMenu=()=>{
+        menuRef.current.style.right="-300px";
+    }
     return <>
         <div className='navbar'>
             <div className='navbar-image'>
@@ -11,16 +20,15 @@ const Navbar = () => {
                 <p>Naman</p>
             </div>
                 <div className='navbar-pics'>
-                <img src={assets.img8} className='navbar-pic1' alt="" />
-                <img src={assets.img9} className='navbar-pic2' alt="" />
+                <img src={assets.img8} onClick={openMenu} className='navbar-pic1' alt="" />
                 </div>
-            
             <div className='navbar-list'>
-                <ul className='navbar-list-info'>
-                    <Link to='home' spy={true} smooth={true} offset={-100} duration={500}><p onClick={() => setPass("home")} className={pass === "home" ? "active" : ""}>Home</p></Link>
-                    <Link to='about' spy={true} smooth={true} offset={-40} duration={200}><p onClick={() => setPass("About")} className={pass === "About" ? "active" : ""}>About</p></Link>
-                    <Link to='projects' spy={true} smooth={true} offset={-40} duration={500}> <p onClick={() => setPass("Projects")} className={pass === "Projects" ? "active" : ""}>Projects</p></Link>
-                    <Link to='contact' spy={true} smooth={true} offset={0} duration={400}><p onClick={() => setPass("Contact us")} className={pass === "Contact us" ? "active" : ""}>Contact us </p></Link>
+                <ul ref={menuRef} className='navbar-list-info'>
+                    <img src={assets.img9}  ref={menuRef} onClick={closeMenu} className='navbar-pic2' alt="" />
+                    <Link to='home' spy={true} ref={menuRef} smooth={true} offset={-100} duration={500}><p onClick={() => setPass("home")} className={pass === "home" ? "active" : ""}>Home</p></Link>
+                    <Link to='about' spy={true}ref={menuRef}  smooth={true} offset={-40} duration={200}><p onClick={() => setPass("About")} className={pass === "About" ? "active" : ""}>About</p></Link>
+                    <Link to='projects' spy={true} ref={menuRef} smooth={true} offset={-40} duration={500}> <p onClick={() => setPass("Projects")} className={pass === "Projects" ? "active" : ""}>Projects</p></Link>
+                    <Link to='contact' spy={true}ref={menuRef}  smooth={true} offset={0} duration={400}><p onClick={() => setPass("Contact us")} className={pass === "Contact us" ? "active" : ""}>Contact us </p></Link>
                 </ul>
             </div>
         </div>
